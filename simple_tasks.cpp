1ed6e7c177243dc2bf31022cf6e92a7e285ea864
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2026-06-07 20:27:45 +0300

    Update simple_tasks.cpp
    Реверс односвязного списка.

--- simple_tasks.cpp
+++ simple_tasks.cpp
@@ -17,3 +17,78 @@ int main()
 
     return 0;
 }
+
+
+/*******************************************************************************/
+/* Есть структура по типу «односвязный список». Напишите функцию, которая      */
+/* разворачивает список. То есть, первый элемент становится последним, а последний - первым. */
+/*******************************************************************************/
+
+#include <iostream>
+
+struct Node{
+   int data;
+   Node * next;
+    
+  Node(int a) : data {a}, next{nullptr} { };
+    
+};
+
+
+Node *  Invert(Node * head)
+{
+    Node * prev = nullptr; 
+    Node * curr = head; 
+    Node * next = nullptr; 
+    
+    
+    while(curr != nullptr)
+    {
+        next = curr->next;
+        
+        curr->next = prev;
+        
+        prev = curr;
+
+        curr = next;
+        
+    }
+
+   return prev; 
+}
+
+int main()
+{
+    Node * head = new Node(1);
+    
+    head->next = new Node(2);
+    
+    head->next->next = new Node (3);
+    
+    head->next->next->next = new Node (4);
+    
+    head->next->next->next->next = new Node (5);
+    
+    Node * ptr = head;
+    
+    while (ptr != nullptr)
+    {
+        std::cout<< ptr->data<<std::endl;
+        
+        ptr = ptr->next;
+        
+    }
+    
+    ptr = Invert(head);
+    
+    while (ptr != nullptr)
+    {
+        std::cout<< ptr->data<<std::endl;
+        
+        ptr = ptr->next;
+        
+    }
+    
+    
+    return 0;
+}
